24d7e5f64451f5e0723fb854fc213b8d5d3e2a2f
Masaki Kawabata Neto <kawabata@usp.br>
2021-03-05 15:17:51 -0300

    melhorando a visualização do resultado

--- resources/views/partials/docblock.blade.php
+++ resources/views/partials/docblock.blade.php
@@ -3,8 +3,11 @@ $factory = \phpDocumentor\Reflection\DocBlockFactory::createInstance();
 $docblock = $factory->create($m->getDocComment());
 $serializer = new \phpDocumentor\Reflection\DocBlock\Serializer();
 @endphp
-{{-- {{ $serializer->getDocComment($docblock) }}<br> --}}
+@if($showall ?? false)
+<pre>{{ $serializer->getDocComment($docblock) }}</pre><br>
+@else
 {{ Str::limit($docblock->getSummary(),70) }}<br>
+@endif
 {{-- {{ $docblock->getDescription() }}<br>
 @foreach ($docblock->getTags() as $tag)
   {{ $tag }}<br>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,10 +4,17 @@
 
 @section('content')
   <div class="form-inline">
-    Chamada: <b>{{ $classe }}::{{ $metodo }}({{ implode(',', $params) }})</b>
+    <span class="h4"><a href="{{ $classe }}"> {{ $classe }}</a>
+      <i class="fas fa-angle-right"></i> {{ $metodo }}({{ implode(',', $params) }})</span>
     @include('partials.params', ['m'=>$methodReflection])
   </div>
-  <div>Resultado</div>
+  <div>
+    <button class="btn btn-sm btn-primary docblock_btn">Docblock</button>
+    <div class="docblock d-none">
+      @include('partials.docblock',['m'=>$methodReflection, 'showall'=>true])
+    </div>
+  </div>
+  <div class="h4">Resultado</div>
   @if ($type == 'multi_array')
     <table class="table table-stripped table-hover table-bordered datatable-custom no-footer">
       <thead>
@@ -45,13 +52,13 @@
       @endforeach
     </table>
   @endif
-  @if($type == 'string')
-  {{ $data }}
+  @if ($type == 'string')
+    {{ $data }}
   @endif
-  @if($type == 'boolean')
-  Booleano: 
-  @if($data === true) Verdadeiro @endif
-  @if($data === false) Falso @endif
+  @if ($type == 'boolean')
+    Booleano:
+    @if ($data === true) Verdadeiro @endif
+    @if ($data === false) Falso @endif
   @endif
 @endsection
 
@@ -59,16 +66,21 @@
   @parent
   @include('partials.params-js')
   <script>
-      $(document).ready(function() {
-        $('.datatable-custom').DataTable( {
-            dom: 'fitp', // https://datatables.net/examples/basic_init/dom.html
-            paginate: false,
-            "buttons": [
-            'excelHtml5'
-            , 'csvHtml5'
+    $(document).ready(function() {
+      $('.datatable-custom').DataTable({
+        dom: 'fitp', // https://datatables.net/examples/basic_init/dom.html
+        paginate: false,
+        "buttons": [
+          'excelHtml5', 'csvHtml5'
         ]
-        } );
-    } );
+      });
+
+      $('.docblock_btn').click(function() {
+        $('.docblock').toggleClass('d-none')
+      })
+
+    });
+
   </script>
 
 @endsection
