687a637d4bc9211f384cf0a3eb684f24ba72e19d
Masaki Kawabata Neto <kawabata@usp.br>
2021-03-08 10:32:56 -0300

    perfumaria

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,17 +5,20 @@
 @section('content')
 
   <h4>Laravel USP Theme</h4>
+
+  Visualize a página de demo incluída no theme. Há exemplos de uso do datatables, datepicker,
+  select2 e mask.
   <ul>
     <li><a href="theme">Theme</a></li>
   </ul>
 
   <h4>Replicado</h4>
   <ul>
-  @foreach ($classes as $classe)
-    <li><a href="Replicado/{{ substr($classe, strrpos($classe, '\\') +1, strlen($classe)) }}">{{ $classe }}</a></li>
+    @foreach ($classes as $classe)
+      <li><a href="Replicado/{{ substr($classe, strrpos($classe, '\\') + 1, strlen($classe)) }}">{{ $classe }}</a>
+      </li>
     @endforeach
   </ul>
-
 
   <h4>Senhaunica fake</h4>
 
