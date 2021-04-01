b1835a3cfc33e4311ac716a43d47632a4efe74fa
Masaki Kawabata Neto <kawabata@usp.br>
2021-04-01 20:23:47 -0300

    não é necessário config(app.url) como prefixo

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -4,22 +4,20 @@ $right_menu = [
     [
         'text' => '<i class="fas fa-cog"></i>',
         'title' => 'Configurações',
-        'target' => '_blank',
-        'url' => config('app.url') . '/item1',
-        'align' => 'right',
+        'url' => '',
     ],
 ];
 
 # dashboard_url renomeado para app_url
-# USPTHEME_SKIN deve ser colocado no .env da aplicação 
+# USPTHEME_SKIN deve ser colocado no .env da aplicação
 
 return [
     'title' => config('app.name'),
     'skin' => env('USP_THEME_SKIN', 'uspdev'),
     'app_url' => config('app.url'),
     'logout_method' => 'POST',
-    'logout_url' => config('app.url') . '/logout',
-    'login_url' => config('app.url') . '/login',
+    'logout_url' => 'logout',
+    'login_url' =>'login',
     'menu' => [],
     'right_menu' => $right_menu,
 ];
