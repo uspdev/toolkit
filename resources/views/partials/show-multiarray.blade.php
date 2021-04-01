907c79dca03c80095caa730b74cd8a87c04168f6
Masaki Kawabata Neto <kawabata@usp.br>
2021-04-01 09:54:38 -0300

    ajustes para lattes mostrar conteudo array

--- resources/views/partials/show-multiarray.blade.php
+++ resources/views/partials/show-multiarray.blade.php
@@ -12,7 +12,11 @@
       <tr>
         @foreach ($row as $col)
           <td>
-            {{ $col }}
+            @if (is_array($col))
+              <pre>{{ json_encode($col, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+            @else
+              {{ $col }}
+            @endif
           </td>
         @endforeach
       </tr>
--- resources/views/partials/show-simplearray.blade.php
+++ resources/views/partials/show-simplearray.blade.php
@@ -6,7 +6,13 @@
   @foreach ($data as $key => $val)
     <tr>
       <td>{{ $key }}</td>
-      <td>{{ $val }}</td>
+      <td>
+        @if (is_array($val))
+          <pre>{{ json_encode($val, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+        @else
+          {{ $val }}
+        @endif
+      </td>
     </tr>
   @endforeach
 </table>
