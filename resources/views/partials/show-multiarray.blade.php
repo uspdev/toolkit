5c9ea9e9209c41e3ff57255dbac540eb1a849cf5
Masaki Kawabata Neto <kawabata@usp.br>
2021-04-09 16:24:41 -0300

    botão para exportar xls e csv

--- resources/views/partials/show-multiarray.blade.php
+++ resources/views/partials/show-multiarray.blade.php
@@ -1,44 +1,48 @@
 <table class="table table-stripped table-hover table-bordered resultado-multiarray no-footer">
-  <thead>
-    <tr>
-      @foreach ($keys as $key)
-        <th>
-          {{ $key }}
-        </th>
-      @endforeach
-  </thead>
-  <tbody>
-    @foreach ($data as $row)
-      <tr>
-        @foreach ($row as $col)
-          <td>
-            @if (is_array($col))
-              <pre>{{ json_encode($col, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
-            @else
-              {{ $col }}
-            @endif
-          </td>
+    <thead>
+        <tr>
+            @foreach ($keys as $key)
+            <th>
+                {{ $key }}
+            </th>
+            @endforeach
+    </thead>
+    <tbody>
+        @foreach ($data as $row)
+        <tr>
+            @foreach ($row as $col)
+            <td>
+                @if (is_array($col))
+                <pre>{{ json_encode($col, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+                @else
+                {{ $col }}
+                @endif
+            </td>
+            @endforeach
+        </tr>
         @endforeach
-      </tr>
-    @endforeach
-  </tbody>
+    </tbody>
 </table>
 
 @section('javascripts_bottom')
-  @parent
-  <script>
+@parent
+<script>
     $(document).ready(function() {
 
-      oTable = $('.resultado-multiarray').DataTable({
-        dom: 't',
-        "paging": false,
-        "sort": true,
-        "order": [
-          [0, "asc"]
-        ]
-      })
+        oTable = $('.resultado-multiarray').DataTable({
+            dom: 'Bt'
+            , "paging": false
+            , "sort": true
+            , "order": [
+                [0, "asc"]
+            ]
+            , buttons: [
+                'excelHtml5'
+                , 'csvHtml5'
+            ]
+        })
 
     })
 
-  </script>
+</script>
 @endsection
