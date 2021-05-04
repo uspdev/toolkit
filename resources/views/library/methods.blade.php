06827445a9c2183faef9ce22d2f9a9da4125f053
Masaki Kawabata Neto <kawabata@usp.br>
2021-05-04 20:37:56 -0300

    colocando docblock no nome do método
    com tooltip e com click para mostrar

--- resources/views/library/methods.blade.php
+++ resources/views/library/methods.blade.php
@@ -2,59 +2,99 @@
 
 @section('title') Sistema USP @endsection
 
+@section('styles')
+<style>
+    a.nostyle:link {
+        text-decoration: inherit;
+        color: inherit;
+    }
+
+    a.nostyle:visited {
+        text-decoration: inherit;
+        color: inherit;
+    }
+
+    /* https://stackoverflow.com/questions/14596743/how-do-you-change-the-width-and-height-of-twitter-bootstraps-tooltips */
+    .tooltip-inner {
+        max-width: 800px;
+        color: #000;
+        background-color: #17a2b8;
+        border-radius: .25rem;
+        font-size: 15px;
+    }
+
+</style>
+@endsection
+
 @section('content')
-  <div class="card">
+<div class="card">
     <div class="card-header h4">
-      <div class="form-inline">
-        <a href="">Home</a> <i class="fas fa-angle-right mx-1"></i> {{ $classe->getShortName() }}
-        @include('partials.datatable-totalbox')
-        @include('partials.datatable-filterbox')
-      </div>
+        <div class="form-inline">
+            {{ $library }} <i class="fas fa-angle-right mx-1"></i>
+            {{ $classe->getShortName() }}
+            @include('partials.datatable-totalbox')
+            @include('partials.datatable-filterbox')
+        </div>
     </div>
     <div class="card-body">
-      <table class="table table-stripped table-hover table-bordered listar-metodos no-footer">
-        <thead>
-          <tr>
-            <th>
-              Método
-            </th>
-            <th>
-              Docblock</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($classe->getMethods() as $m)
-            <tr>
-              <td class="form-inline">
-                {{ $m->getName() }} @include('library.partials.params')
-              </td>
-              <td style="width:40%">
-                @include('partials.docblock')
-              </td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
+        <table class="table table-stripped table-hover table-bordered listar-metodos no-footer">
+            <thead>
+                <tr>
+                    <th>Método</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($classe->getMethods() as $m)
+                <tr>
+                    <td>
+                        <div class="form-inline">
+                            {{-- vamos ajudar o datatables a ordenar --}}
+                            <span style="display:none">{{ $m->getName() }}</span>
+
+                            <a href="#" class="docblock_btn nostyle" data-toggle="tooltip" data-placement="top" title="@include('partials.docblock')">
+                                {{ $m->getName() }}
+                            </a>
+                            @include('library.partials.params')
+                        </div>
+                        <div class="docblock my-2 docblock_btn" style="display:none">
+                            @include('partials.docblock',['showall'=>true])
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-  </div>
+</div>
 
 @endsection
 
 @section('javascripts_bottom')
-  @parent
-  <script>
+@parent
+<script>
     $(document).ready(function() {
 
-      oTable = $('.listar-metodos').DataTable({
-        dom: 't',
-        "paging": false,
-        "sort": true,
-        "order": [
-          [0, "asc"]
-        ]
-      })
+        oTable = $('.listar-metodos').DataTable({
+            dom: 't'
+            , "paging": false
+            , "sort": true
+            , "order": [
+                [0, "asc"]
+            ]
+        })
+
+        // Botão para mostrar o docblock
+        $('.docblock_btn').click(function(e) {
+            e.preventDefault()
+            $(this).closest('td').find('.docblock').slideToggle()
+        })
+
+        $('[data-toggle="tooltip"]').tooltip({
+            html: true
+            , boundary: 'window'
+        })
 
     })
 
-  </script>
+</script>
 @endsection
--- resources/views/library/partials/params.blade.php
+++ resources/views/library/partials/params.blade.php
@@ -1,24 +1,22 @@
-@if($m->isPublic()) 
+@if($m->isPublic())
 <form class="form-inline" method="POST" action="library/{{ $library }}/{{ $classe->getShortName() }}/{{ $m->getName() }}">
-  @csrf
-  @forelse ($m->getParameters() as $p)
-    @php
-      $required = $p->isOptional() ? '' : 'required';
-    @endphp
+    @csrf
+    @forelse ($m->getParameters() as $p)
+    @php( $required = $p->isOptional() ? '' : 'required' )
     <div class="input-group input-group-sm mx-sm-2">
-      @if (!$p->isOptional())
+        @if (!$p->isOptional())
         <div class="input-group-prepend">
-          <div class="input-group-text"><i class="fas fa-asterisk text-danger"></i></div>
+            <div class="input-group-text"><i class="fas fa-asterisk text-danger"></i></div>
         </div>
-      @endif
-      <input class="form-control" type="text" name="{{ $p->name }}" 
-      placeholder="{{ $p->name }}" value=""
-        {{ $required }}> @include('partials.param-default-value')
+        @endif
+        <input class="form-control" type="text" name="{{ $p->name }}" placeholder="{{ $p->name }}" value="" {{ $required }}> @include('partials.param-default-value')
     </div>
     @empty
-<span class="badge badge-primary mx-2">Sem parâmetros</span>
-  @endforelse
-  <button class="btn submit_btn p-0"><i class="fas fa-lg fa-chevron-circle-right text-success"></i></button>
+    <span class="badge badge-primary mx-2">Sem parâmetros</span>
+    @endforelse
+    <button class="btn submit_btn p-0" data-toggle="tooltip" data-placement="right" title="Executa o método">
+        <i class="fas fa-lg fa-chevron-circle-right text-success"></i>
+    </button>
 </form>
 @else
 <span class="badge badge-warning ml-2">Método privado</span>
