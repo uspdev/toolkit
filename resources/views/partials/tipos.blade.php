a2013008813c95b921b39f59c4a2d1754babfe0a
Masaki Kawabata Neto <kawabata@usp.br>
2021-05-04 15:32:19 -0300

    removendo espaços entre os parenteses

--- resources/views/partials/tipos.blade.php
+++ resources/views/partials/tipos.blade.php
@@ -1,16 +1,14 @@
-(
 @switch($type)
-  @case('multi_array')
-  Lista
-  @break
-  @case('simple_array')
-  Registro
-  @break
-  @case('string')
-  Valor
-  @break
-  @case('boolean')
-  Verdadeiro/Falso
-  @break
+@case('multi_array')
+(Lista)
+@break
+@case('simple_array')
+(Registro)
+@break
+@case('string')
+(Valor)
+@break
+@case('boolean')
+(Verdadeiro/Falso)
+@break
 @endswitch
-)
