afc651e8a44523dcc32d4a209af12ae08357ef38
Masaki Kawabata Neto <kawabata@usp.br>
2021-05-04 20:37:06 -0300

    ajustes menores

--- resources/views/partials/docblock.blade.php
+++ resources/views/partials/docblock.blade.php
@@ -1,17 +1,17 @@
 @php
 $factory = \phpDocumentor\Reflection\DocBlockFactory::createInstance();
-$comment = $m->getDocComment() ? $m->getDocComment() : '--vazio--';
+$comment = $m->getDocComment() ? $m->getDocComment() : '-- vazio --';
 $docblock = $factory->create($comment);
-$serializer = new \phpDocumentor\Reflection\DocBlock\Serializer();
+//$serializer = new \phpDocumentor\Reflection\DocBlock\Serializer();
 @endphp
 @if ($showall ?? false)
   <div class="card bg-info text-white">
     <div class="card-body">
-      <pre>{{ $comment }}</pre><br>
+      <pre>{{ $comment }}</pre>
     </div>
   </div>
 @else
-  {{ Str::limit($docblock->getSummary(), 70) }}<br>
+  {!! Str::limit($docblock->getSummary(), 120) !!}
 @endif
 {{-- {{ $docblock->getDescription() }}<br>
 @foreach ($docblock->getTags() as $tag)
